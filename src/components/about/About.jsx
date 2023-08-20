a67d5863fc83bb3da2212a2d64a5ce1a169734a7
iamsuryakant <suryakantthakur825@gmail.com>
2023-08-20 20:31:46 +0530

    added initial-codes

---
+++ src/components/about/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+      <section className= 'py-10' id='about'>
+          <div className='py-20'>
+              <div className='flex justify-center items-center'>
+                  <span className='text-black text-2xl font-bold pb-0.5'>About Me</span>
+              </div>
+          </div>
+    </section>
+  )
+}
+
+export default About
